8692dc4bcd068039e3a914d668ba3dff44c3906e
vladxdd <vladcorcodel12@mail.ru>
2023-04-08 14:33:03 +0300

    "hz"

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
   <!-- Favicons -->
   <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/favicon.png" rel="favicon">
+  <link href="assets/img/favicon.png" rel="favicon.png">
 
   <!-- Google Fonts -->
   <link
